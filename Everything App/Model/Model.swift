a9a92d73ba4ca6a9fa0d9a87d8609633dcb84e7b
Гость <Guest@mbp078.local>
2019-04-13 12:49:35 +0300

    Added UI and code

---
+++ Everything App/Model/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  Everything App
+//
+//  Created by Гость on 13/04/2019.
+//  Copyright © 2019 Гость. All rights reserved.
+//
+
+import UIKit
+
+struct Model {
+    let title: String
+    let detail: String
+    let image: UIImage?
+}
---
+++ View Controllers/TableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TableViewController.swift
+//  Everything App
+//
+//  Created by Гость on 13/04/2019.
+//  Copyright © 2019 Гость. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    var models = [Model]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        models = [
+            Model(title: "Барабан маленький", detail: "Маленький барабан будет отличным подарком вашим знакомым, отлично украсит ваш офис или квартиру. Имеет красивую роспись, с нанесением этнического орнамента. Изготовлен барабан из бамбука и кожи настоящей обезьяны. Материал: бамбук, кожа обезьяны", image: UIImage(named: "baraban-malenkiy-reznoy_enl-1"))
+        ]
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return view.bounds.height / 8
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SubtitleCellIdentifier")!
+        let model = models[indexPath.row]
+        
+        configure(cell, with: model)
+        
+        return cell
+    }
+    
+    func configure(_ cell: UITableViewCell, with model: Model) {
+        cell.textLabel?.text = model.title
+        cell.detailTextLabel?.text = model.detail
+        cell.imageView?.image = model.image
+        
+    }
+    
+}
