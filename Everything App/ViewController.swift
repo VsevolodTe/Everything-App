296c3274884ff90f7383fc9c938c880acc37570d
Гость <Guest@mbp078.local>
2019-04-13 11:21:08 +0300

    Initial Commit

---
+++ Everything App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Everything App
+//
+//  Created by Гость on 13/04/2019.
+//  Copyright © 2019 Гость. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
